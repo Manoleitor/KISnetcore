509002e4867f34c1a4c8b378a14472bdf4f74108
manolo <manuel666@gmail.com>
2022-09-15 17:27:17 +0200

    format code

--- Classes/CursorPosition.cs
+++ Classes/CursorPosition.cs
@@ -1,10 +1,14 @@
-namespace Classes{
-public class CursorPosition{
-    public int PosX {
-        get; set;
+namespace Classes
+{
+    public class CursorPosition
+    {
+        public int PosX
+        {
+            get; set;
+        }
+        public int PosY
+        {
+            get; set;
+        }
     }
-    public int PosY{
-        get; set;
-    }
-}
 }
